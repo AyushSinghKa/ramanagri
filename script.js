b673bc5bfbf063caae03b5ea5aeb9ce86c391a86
AyushSinghKa <0998ayushsingh@gmail.com>
2026-01-06 00:18:01 +0530

    1st try

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("bookingForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const pickup = document.getElementById("pickup").value;
+  const destination = document.getElementById("destination").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+  const service = document.getElementById("service").value;
+
+  const message = `*Ayodhya Booking Request*
+
+Name        : ${name}
+Phone       : ${phone}
+Service     : ${service}
+
+Pickup      : ${pickup}
+Destination : ${destination}
+
+Date        : ${date}
+Time        : ${time}
+
+Please confirm availability.
+|| Jai Shri Ram ||`;
+
+  const whatsappNumber = "918595941579"; // CHANGE NUMBER HERE
+  const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+
+  window.open(url, "_blank");
+});
